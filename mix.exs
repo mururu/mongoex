4bd4d8e5bb35f48fb1244ac044dc46c896686822
Yuki Ito <yuki@gnnk.net>
2013-07-13 21:27:33 +0900

    Use fork mongodb-erlang

--- mix.exs
+++ mix.exs
@@ -15,7 +15,6 @@ defmodule Mongoex.Mixfile do
   # Returns the list of dependencies in the format:
   # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    [ { :mongodb, github: "mongodb/mongodb-erlang" },
-      { :bson,    github: "mongodb/bson-erlang" } ]
+    [ { :mongodb, github: "mururu/mongodb-erlang" } ]
   end
 end
